b0ead526fb50608701b790a7860e814d97de4a2c
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2017-11-10 23:28:56 +0700

    pertemuan 20

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -10,7 +10,7 @@ class AdminLoginController extends Controller
 {
   public function __construct()
   {
-      $this->middleware('guest:admin');
+      $this->middleware('guest:admin', ['except'=> ['logout']]);
   }
 
   function showLoginForm()
@@ -36,4 +36,9 @@ class AdminLoginController extends Controller
     return redirect()->back()->withInput($request->only('email', 'remember'));
   }
 
+  public function logout()
+  {
+    Auth::guard('admin')->logout();
+    return redirect('/');
+  }
 }
