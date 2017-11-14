ce6f33bd370c1d5cb47588044aa914181cbb0902
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2017-11-14 12:49:37 +0700

    pertemuan 21

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
 
 Route::prefix('admin')->group(function() {
+  Route::resource('manageadmins', 'ManageAdminController');
+
   Route::get('/', 'AdminController@index')->name('admin.dashboard');
   Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
   Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
